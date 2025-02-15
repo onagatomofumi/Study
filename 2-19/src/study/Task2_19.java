b077d0689c75fcf15348a789cd2e4c85b76972eb
onaga tomofumi <onaga.tomofumi.20230801@gmail.com>
2025-02-16 05:12:31 +0900

    2-19

---
+++ 2-19/src/study/Task2_19.java
@@ -0,0 +1,26 @@
+package study;
+/**
+* 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
+*
+* 問①〜問③まであります。
+* 全て回答し、課題にある画像と同じ表示になるようにしてください。
+*
+*/
+
+
+//②Fruits.javaをインポートしなさい。
+import study.Fruits.Fruits;
+//①FruitsConstants.javaをインポートしなさい。
+import study.FruitsConstants.FruitsConstants;
+
+public class Task2_19 {
+    public static void main(String[] args) {
+        /* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
+        * また、実行結果がターミナルと同じ表示になるように
+        * printFruitsの引数にはFruitsConstantsに指定されている定数を設定してください。
+        */
+        Fruits.printFruits(FruitsConstants.FRUITS_LEMON_04, FruitsConstants.FRUITS_COUNT_20);
+        
+        }
+
+}
